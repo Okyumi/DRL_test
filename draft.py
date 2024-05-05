ee294aaccfb6f9d3a57e11c6c76794772ef9fa6a
Yumi Omori <omoriyumi8@gmail.com>
2024-05-05 19:37:12 +0400

    new version.

---
+++ draft.py
@@ -0,0 +1,16 @@
+int modifiedbinsearch(int x, int V[], int n){
+    int low, high, mid;
+    low=0;
+    high = n-1;
+    while(low <= high){
+        mid = (low + high)/2
+        if (X < V[mid]){
+            high = mid - 1;
+            mid = mid - 1;
+        }
+        else if (X > V[mid])
+            low = mid + 1;
+        else
+            
+    }
+}
